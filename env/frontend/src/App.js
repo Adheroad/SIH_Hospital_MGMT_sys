a2c9594c33acf6570108272f6a38566fcb6022b9
Adheroad <anshuldhawanad@gmail.com>
2024-09-08 02:01:52 +0530

    1. Functional Backend and Frontend 2. Fixed wrrongly pushed backend

---
+++ env/frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Button } from '@mui/material';
+
+function App() {
+  return (
+    <div>
+      <h1>Hospital Management System</h1>
+      <Button variant="contained" color="primary">
+        SOS Ambulance
+      </Button>
+    </div>
+  );
+}
+
+export default App;
