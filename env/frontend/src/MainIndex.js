ba38365da38026d5b18fa02508a62083cba25db9
Ritwik-Bug <ritwik450@gmail.com>
2024-09-12 01:10:00 +0530

    Frontend set up

---
+++ env/frontend/src/MainIndex.js
@@ -0,0 +1,11 @@
+import React from "react";
+// import './App.css';
+function MainIndex() {
+  return (
+    <div className="body">
+      testing main index
+    </div>
+  );
+}
+
+export default MainIndex;
